73b4a03e27ca4330d7c96e7c49d971f768d3fc20
Aviel Naftali <68643310+orangdong@users.noreply.github.com>
2022-06-14 13:08:44 +0800

    finish model

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const result = document.getElementById('result');
-result.innerHTML = 'This is from js'
+const predictBtn = document.getElementById('predict-btn');
+
+const loadModel = async () => {
+    const model = undefined;
+    const model = await tf.loadLayersModel('https://raw.githubusercontent.com/orangdong/keras-test/master/model.json');
+    console.log("model loaded");
+}
+loadModel();
+
+predictBtn.addEventListener('click', () => {
+    const dummyHotel = [0, 1, 2, 3, 4];
+    const dummyUser = [1, 1, 1, 1, 1];
+    const data = tf.tensor2d([
+        dummyHotel, dummyUser
+    ]);
+    const output = model.predict(data);
+    const prediction = output.dataSync();
+    console.log(prediction)
+    result.innerHTML = prediction
+})
